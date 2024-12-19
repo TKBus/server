d2d8807f25f6219c659540bfb6ce2969f0332e5c
김회창 <khc9812121@gmail.com>
2024-12-19 15:08:37 +0900

    github action 이번에는 작동하니?

--- src/main/java/com/tkbus/app/AppApplication.java
+++ src/main/java/com/tkbus/app/AppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
-		// github action 변화 테스트
+		// github action 변화 테스트1
 		SpringApplication.run(AppApplication.class, args);
 	}
 
