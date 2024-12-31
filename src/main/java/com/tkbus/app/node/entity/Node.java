1c0f30704dc3de91ef8a9c7ec6ac71aed1dd76b3
김회창 <khc9812121@gmail.com>
2024-12-31 13:27:25 +0900

    정류소 정보에 도시 정보 추가로 인한 Node 엔티티 수정

--- src/main/java/com/tkbus/app/node/entity/Node.java
+++ src/main/java/com/tkbus/app/node/entity/Node.java
@@ -4,14 +4,20 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.tkbus.app.city.entity.City;
 import com.tkbus.app.noderoute.entity.NodeRoute;
 
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 
@@ -23,6 +29,10 @@ public class Node {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "city_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private City city;
 
 	private String code;
 
--- src/main/java/com/tkbus/app/node/repository/NodeRepository.java
+++ src/main/java/com/tkbus/app/node/repository/NodeRepository.java
@@ -1,10 +1,14 @@
 package com.tkbus.app.node.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tkbus.app.city.entity.City;
 import com.tkbus.app.node.entity.Node;
 
 @Repository
 public interface NodeRepository extends JpaRepository<Node, Integer> {
+	List<Node> findByCity(City city);
 }
